5c83d996e299826422ade3ee6ec6c0cc1787b5a4
Web <web@it.step>
2022-10-27 21:25:22 +0300

    add theory

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 "use strict";
-let a = 5;
+// let arr: 'asd' = 'asd';
+// let arr: string[] = [];
+// let arr: [string, boolean?, number?] = ['hi', false];
+// arr.push('adads'); // ошибка нарушение картежа
+var Feature;
+(function (Feature) {
+    Feature[Feature["baner"] = 0] = "baner";
+    Feature[Feature["adminka"] = 1] = "adminka";
+    Feature[Feature["premium"] = 2] = "premium";
+})(Feature || (Feature = {}));
+const a = Feature.adminka;
+let b = Feature.premium;
+b = 0;
 console.log(a);
+// let arr: Array<string> = [];
+// let arr2: string[] = [];
+// const person: {
+//     name: string;
+//     age: number;
+//     city?: string;
+// } = {
+//     name: "John",
+//     age: 23,
+// };
+// person.age = 44;
+// person.city = 'asd';
--- index.ts
+++ index.ts
@@ -1,3 +1,84 @@
-let a = 5;
+// let arr: 'asd' = 'asd';
+// let arr: string[] = [];
+// let arr: [string, boolean?, number?] = ['hi', false];
+// arr.push('adads'); // ошибка нарушение картежа
 
-console.log(a);
+// enum Feature{
+//     baner,
+//     adminka,
+//     premium,
+// }
+
+// const a = Feature.adminka;
+// let b: Feature = Feature.premium;
+// b = 0;
+// console.log(a);
+
+// let a: any = 2;
+// a = "asd";
+// let b: unknown = 2;
+// b = 'aadfsfs';
+
+// if(typeof b === 'string'){
+//     b.toUpperCase();
+// }
+
+
+// let arr: Array<string> = [];
+// let arr2: string[] = [];
+
+// const person: {
+//     name: string;
+//     age: number;
+//     city?: string;
+// } = {
+//     name: "John",
+//     age: 23,
+// };
+
+// person.age = 44;
+// person.city = 'asd';
+
+
+// const person = {
+//     name: 'John',
+//     age: 23,
+// }
+
+// const person2: Person = {
+//     name: 'Valerchik',
+//     age: 21,
+// }
+
+// let id: number | string | boolean = '123456'
+// id = 123
+
+// let a: string & number = 2;
+
+
+// type Person = {
+//     name: string;
+//     age: number;
+//     city?: string;
+// };
+
+
+// type Admin = {
+//     puzo: boolean,
+//     age: string,
+//     beerLiters: number
+// }
+
+// let person: Person | Admin = {
+//     name: 'asd',
+//     age: '23',
+//     puzo: false,
+//     beerLiters: 23
+// }
+
+// function foo(a: number, b: number){
+//     return a + b;
+// }
+
+// const res = foo(2, 3);
+
